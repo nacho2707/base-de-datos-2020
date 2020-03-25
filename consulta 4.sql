1e2be64383efe83c4c4f2d1d7dabe926b5b32bb2
nacho2707 <49006792+nacho2707@users.noreply.github.com>
2020-03-25 08:26:04 -0400

    Update consulta 4.sql

--- consulta 4.sql
+++ consulta 4.sql
@@ -1,3 +1,5 @@
 select count(idcurso), matricula
 from estudiantecurso
 group by matricula
+having count(idcurso)>4
+-- ya que la condicion esq que estudiantes no pasen mas de 4 mterias, esta tabla siempre deberia estar vacia
